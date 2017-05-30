58122cdf4c4803f91cdc1d0532d8fcecad7d2936
Vandan M. Shah <vandanshah41@gmail.com>
2017-05-30 23:36:27 +0530

    Add files via upload

---
+++ Enable_Disable_Registry/bin/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Enable_Disable_Rigistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_disable = new System.Windows.Forms.Button();
+            this.btn_enable = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enable-Disable Task Manager";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(335, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 74);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Disable TaskManager";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(72, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enable TaskManager";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(764, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enable-Disable Startup Software";
+            // 
+            // btn_disable
+            // 
+            this.btn_disable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_disable.Location = new System.Drawing.Point(638, 394);
+            this.btn_disable.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_disable.Name = "btn_disable";
+            this.btn_disable.Size = new System.Drawing.Size(227, 74);
+            this.btn_disable.TabIndex = 8;
+            this.btn_disable.Text = "Disable Control Panel";
+            this.btn_disable.UseVisualStyleBackColor = true;
+            this.btn_disable.Click += new System.EventHandler(this.btn_disable_Click);
+            // 
+            // btn_enable
+            // 
+            this.btn_enable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_enable.Location = new System.Drawing.Point(373, 394);
+            this.btn_enable.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_enable.Name = "btn_enable";
+            this.btn_enable.Size = new System.Drawing.Size(227, 74);
+            this.btn_enable.TabIndex = 7;
+            this.btn_enable.Text = "Enable Control Panel";
+            this.btn_enable.UseVisualStyleBackColor = true;
+            this.btn_enable.Click += new System.EventHandler(this.btn_enable_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(497, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enable-Disable Control Panel";
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(905, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(237, 74);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Disable Startup Software";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(638, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 74);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Enable Startup Software";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 530);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_disable);
+            this.Controls.Add(this.btn_enable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_disable;
+        private System.Windows.Forms.Button btn_enable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Enable_Disable_Registry/bin/Form1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enable_Disable_Rigistry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey regkey = default(RegistryKey);
+            string keyValueInt = "-1";
+            //0x00000000 (0)
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                //regkey.DeleteSubKey("DisableTaskMgr");
+                regkey.SetValue("DisableTaskMgr", keyValueInt);
+                regkey.Close();
+                MessageBox.Show("You Have enabled Task Manager....");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Interaction.MsgBox(ex.Message, MsgBoxStyle.Critical, "Registry Error!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey regkey = default(RegistryKey);
+            string keyValueInt = "1";
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                regkey.SetValue("DisableTaskMgr", keyValueInt);
+                regkey.Close();
+                MessageBox.Show("You Have disabled Task Manager....");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Interaction.MsgBox(ex.Message, MsgBoxStyle.Critical, "Registry Error!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey localMachine = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+                //localMachine = RegistryKey.OpenRemoteBaseKey(baseRegistryKey, remoteIP);
+                RegistryKey regKey = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
+                regKey.SetValue("Userinit", "C:\\Program Files\\EMS\\EMS\\Demo.exe", RegistryValueKind.String);
+                regKey.Close();
+                MessageBox.Show("You Have enabled startup software....");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Interaction.MsgBox(ex.Message, MsgBoxStyle.Critical, "Registry Error!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                RegistryKey localMachine = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+
+                RegistryKey regKey = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
+                regKey.SetValue("Userinit", "C:\\Windows\\System32\\userinit.exe", RegistryValueKind.String);
+                regKey.Close();
+                MessageBox.Show("You Have disabled startup software....");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Interaction.MsgBox(ex.Message, MsgBoxStyle.Critical, "Registry Error!");
+            }
+        }
+
+        private void btn_enable_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Win32.RegistryKey regkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            regkey.SetValue("NoControlPanel", false, Microsoft.Win32.RegistryValueKind.DWord);
+            regkey.Close();
+
+            regkey = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            regkey.SetValue("NoControlPanel", false, Microsoft.Win32.RegistryValueKind.DWord);
+            regkey.Close();
+            MessageBox.Show("You Have enabled control panel....");
+        }
+
+        private void btn_disable_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey regkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            regkey.SetValue("NoControlPanel", true, Microsoft.Win32.RegistryValueKind.DWord);
+            regkey.Close();
+
+            regkey = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            regkey.SetValue("NoControlPanel", true, Microsoft.Win32.RegistryValueKind.DWord);
+            regkey.Close();
+            MessageBox.Show("You Have disabled control panel....");
+        }
+    }
+}
